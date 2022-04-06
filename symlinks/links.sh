83c148cea5ba462bc98fe959f9c9bf55de872809
Roberto Bocio Melo <robertobocio@01lab.co>
2022-04-06 09:19:02 -0500

    [ADD] alacritty emulator terminal

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
-rm -rf ~/.config/Code/User ~/.gitignore_global ~/.gitconfig ~/.config/starship.toml ~/.zshenv ~/.zshrc ~/.zimrc ~/.bashrc ~/.gitattributes
+# rm -rf ~/.config/alacritty/alacritty.yml  ~/.config/Code/User ~/.gitignore_global ~/.gitconfig ~/.config/starship.toml ~/.zshenv ~/.zshrc ~/.zimrc ~/.bashrc ~/.gitattributes
 # ~/.ssh
 ln -s "$HOME/.dotfiles/terminal/bash/.bashrc" "$HOME/.bashrc"
 ln -s "$HOME/.dotfiles/terminal/zsh/.zimrc" "$HOME/.zimrc"
@@ -9,5 +9,5 @@ ln -s "$HOME/.dotfiles/terminal/zsh/starship.toml" "$HOME/.config/starship.toml"
 ln -s "$HOME/.dotfiles/git/.gitconfig" "$HOME/.gitconfig"
 ln -s "$HOME/.dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
 ln -s "$HOME/.dotfiles/vs-code/User" "$HOME/.config/Code/User"
-# ln -s "$HOME/.dotfiles/.ssh" "$HOME/.ssh"
+ln -s "$HOME/.dotfiles/terminal/emulators/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
 ln -s "$HOME/.dotfiles/git/.gitattributes" "$HOME/.gitattributes"
