5dbcaa1669b78b4fb5d510cc42f68c1b84311b13
Roberto Bocio Melo <robertobocio@01lab.co>
2024-08-02 18:48:46 -0500

    chore: Update paths in exports.sh

--- terminal/_exports/exports.sh
+++ terminal/_exports/exports.sh
@@ -3,26 +3,18 @@ export FZF_DEFAULT_OPTS='
   --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
   --reverse
 '
-# export NAVI_PATH="$DOTFILES_PATH/doc/navi"
 
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-# export DENO_INSTALL="/home/rsbmk/.deno"
-# export PATH="$DENO_INSTALL/bin:$PATH"
-# export ZIM_HOME="$HOME/.zim"
 paths=(
   "$HOME/bin"
   "$DOTFILES_PATH/bin"
   "/bin"
   "/usr/local/bin" # This contains Brew ZSH. If it's below `/bin` it won't be executed.
-  "/Users/robertosamuelbociomelo/.fly/bin"
   "/usr/bin"
-  "/usr/local/bin/Code"
-  "/Users/robertosamuelbociomelo/.deno/bin"
+  "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
   "/usr/bin:/bin:/usr/sbin:/sbin"
-  "/Users/robertosamuelbociomelo/.turso"
-  "/Users/robertosamuelbociomelo/Library/Application Support/fnm"
   "/usr/local/bin:/usr/local/sbin:$PATH"
   "$HOME/.cargo/bin"
 )
