4cc31e7e49b681bb70e3f590f17d3c5c9b1e9d7c
Roberto Bocio Melo <robertobocio@01lab.co>
2024-08-02 19:25:38 -0500

    chore: Refactor install.sh script to handle missing dependencies and generate locale if needed

--- install.sh
+++ install.sh
@@ -4,27 +4,6 @@
 REPO_URL="https://github.com/rsbmk/.dotfiles"
 INIT_SCRIPT="terminal/init.sh"
 CLONE_DIR="$HOME/.dotfiles"
-SCRIPT_URL="https://raw.githubusercontent.com/rsbmk/.dotfiles/master/install_dotfiles.sh"
-
-# Generate locale if needed
-if ! locale -a | grep -q "en_US.utf8"; then
-  echo "Generating en_US.UTF-8 locale..."
-  if command -v locale-gen &>/dev/null; then
-    sudo locale-gen en_US.UTF-8
-  elif [ -f /etc/locale.gen ]; then
-    sudo sed -i 's/^# en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
-    sudo locale-gen
-  elif [ -f /etc/default/locale ]; then
-    echo "LANG=en_US.UTF-8" | sudo tee -a /etc/default/locale
-    sudo dpkg-reconfigure --frontend=noninteractive locales
-  else
-    echo "Cannot generate locale. Please ensure the system has locale support."
-    exit 1
-  fi
-  sudo update-locale LANG=en_US.UTF-8
-else
-  echo "Locale en_US.UTF-8 already exists."
-fi
 
 # Clone the repository
 if [ -d "$CLONE_DIR" ]; then
