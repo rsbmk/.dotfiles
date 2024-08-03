12bd5852f9ad0cdcfc20c5f832f2303911319b53
Roberto Bocio Melo <robertobocio@01lab.co>
2024-08-02 19:19:03 -0500

    chore: Update install.sh script to handle missing dependencies and generate locale if needed

--- install.sh
+++ install.sh
@@ -4,11 +4,36 @@
 REPO_URL="https://github.com/rsbmk/.dotfiles"
 INIT_SCRIPT="terminal/init.sh"
 CLONE_DIR="$HOME/.dotfiles"
+SCRIPT_URL="https://raw.githubusercontent.com/rsbmk/.dotfiles/master/install_dotfiles.sh"
+
+# Check if wget or curl is installed
+if ! command -v curl &>/dev/null; then
+  apt install curl -y
+fi
 
 # Check if git is installed
 if ! command -v git &>/dev/null; then
-  echo "Git is not installed. Please install Git and try again."
-  exit 1
+  apt install git -y
+fi
+
+# Generate locale if needed
+if ! locale -a | grep -q "en_US.utf8"; then
+  echo "Generating en_US.UTF-8 locale..."
+  if command -v locale-gen &>/dev/null; then
+    sudo locale-gen en_US.UTF-8
+  elif [ -f /etc/locale.gen ]; then
+    sudo sed -i 's/^# en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+    sudo locale-gen
+  elif [ -f /etc/default/locale ]; then
+    echo "LANG=en_US.UTF-8" | sudo tee -a /etc/default/locale
+    sudo dpkg-reconfigure --frontend=noninteractive locales
+  else
+    echo "Cannot generate locale. Please ensure the system has locale support."
+    exit 1
+  fi
+  sudo update-locale LANG=en_US.UTF-8
+else
+  echo "Locale en_US.UTF-8 already exists."
 fi
 
 # Clone the repository
